bd3de8be807321ae5c7fb55ba0d363a9dfae5844
Ivo Pereira <ivopereira@ufp.edu.pt>
2023-04-18 15:45:59 +0100

    Make the application work with GitHub Actions + AWS EB

--- application.go
+++ application.go
@@ -44,7 +44,7 @@ func main() {
 
 // hello responds with a default Hello World message in JSON.
 func hello(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Hello World! I'm alive!")
+	c.IndentedJSON(http.StatusOK, "Hello World! I'm alive at AWS EB!")
 }
 
 // getAlbums responds with the list of all albums as JSON.
