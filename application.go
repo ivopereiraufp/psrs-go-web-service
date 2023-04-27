87f17972c3aee605a9ba08bcd205fbcd1f5dfdb2
Ivo Pereira <ivopereira@ufp.edu.pt>
2023-04-27 21:55:36 +0100

    Testing Render

--- application.go
+++ application.go
@@ -44,7 +44,7 @@ func main() {
 
 // hello responds with a default Hello World message in JSON.
 func hello(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Hello World! I'm testing AWS EB for the students")
+	c.IndentedJSON(http.StatusOK, "Hello World! I'm testing Render")
 }
 
 // getAlbums responds with the list of all albums as JSON.
